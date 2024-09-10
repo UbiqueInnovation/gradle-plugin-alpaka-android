eb4c46705febbd21f6e3324bb545d39f5f3f4f63
Mike Wong <wong@ubique.ch>
2024-09-10 16:29:02 +0200

    Add a lot of functionality from old deus-ex plugin

--- plugin-build/plugin/src/main/java/ch/ubique/linth/network/json/UploadDataJson.kt
+++ plugin-build/plugin/src/main/java/ch/ubique/linth/network/json/UploadDataJson.kt
@@ -4,18 +4,19 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class UploadDataJson(
+	val uploadKey: String,
 	val name: String,
 	val packageName: String,
 	val flavor: String,
-	val branch: String,
+	val version: String,
+	val signature: String,
 	val minSdk: Int,
 	val targetSdk: Int,
 	val usesFeature: List<String>,
+	val branch: String,
+	val changelog: String,
+	val buildId: String,
 	val buildNumber: Long,
 	val buildTime: Long,
 	val buildBatch: String,
-	val changelog: String,
-	val signature: String,
-	val version: String,
-	val uploadKey: String,
 )

7d7fedd1237d247e872245336f7bffcb4b34f294
Mike Wong <wong@ubique.ch>
2024-09-12 13:33:32 +0200

    Fix git command execution

--- plugin-build/plugin/src/main/java/ch/ubique/linth/common/GitUtils.kt
+++ plugin-build/plugin/src/main/java/ch/ubique/linth/common/GitUtils.kt
@@ -1,30 +1,40 @@
 package ch.ubique.linth.common
 
+import org.gradle.api.Project
 import java.io.BufferedReader
 
 object GitUtils {
 
-	fun obtainBranch(): String {
-		val cmdGitBranch = "git rev-parse --abbrev-ref HEAD"
-		val process = ProcessBuilder(*cmdGitBranch.split(" ").toTypedArray())
+	fun obtainBranch(project: Project): String {
+		val gitCommand = listOf(
+			"git",
+			"rev-parse",
+			"--abbrev-ref",
+			"HEAD",
+		)
+		val process = ProcessBuilder(gitCommand)
+			.directory(project.rootProject.projectDir)
 			.redirectErrorStream(true)
 			.start()
 
-		var branchName = process.inputStream.bufferedReader().use(BufferedReader::readText).trim()
-		if (branchName.isEmpty()) {
-			branchName = "develop"
-		}
-		return branchName
+		val branchName = process.inputStream.bufferedReader().use(BufferedReader::readText).trim()
+		return branchName.takeIf { it.isNotEmpty() } ?: "develop"
 	}
 
-	fun obtainLastCommits(numOfCommits: Int = 10): String {
-		val lastCommits = "git -C ./ log -$numOfCommits --pretty=format:\"• %s\" --no-merges"
-		val process = ProcessBuilder(*lastCommits.split(" ").toTypedArray())
+	fun obtainLastCommits(project: Project, numOfCommits: Int = 10): String {
+		val gitCommand = listOf(
+			"git",
+			"log",
+			"-$numOfCommits",
+			"--pretty=format:%s (%cn)",
+			"--no-merges",
+		)
+		val process = ProcessBuilder(gitCommand)
+			.directory(project.rootProject.projectDir)
 			.redirectErrorStream(true)
 			.start()
 
-		val last10Commits = process.inputStream.bufferedReader().use(BufferedReader::readText).trim()
-		return last10Commits
+		return process.inputStream.bufferedReader().use(BufferedReader::readText).trim()
 	}
 
 }
